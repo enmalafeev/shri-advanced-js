7e36d9d475f81d149bc7c2c4e31c61584a86a3fc
Evgeny Malafeev <malafeev.en@yandex.ru>
2021-11-04 14:13:13 +0300

    add methods has, size, clear, add

--- solution/index.js
+++ solution/index.js
@@ -1,3 +1,54 @@
 module.exports = class {
-  // реализация
+  // static _uniq(coll) {
+  //   const prims = { "boolean": {}, "number": {}, "string": {} }, objs = [];
+
+  //   return coll.filter((item) => {
+  //     const type = typeof item;
+  //     if (type in prims)
+  //       return prims[type].hasOwnProperty(item) ? false : (prims[type][item] = true);
+  //     else {
+  //       return objs.indexOf(item) >= 0 ? false : objs.push(item);
+  //     }
+  //   });
+  // }
+
+  constructor(collection = []) {
+    // const uniqCollection = this.constructor._uniq(collection);
+    this.collection = collection;
+  }
+
+  
+  [Symbol.iterator] = function() {
+    let index = 0;
+    let collection = this.collection;
+    let length = this.collection.length;
+
+    return {
+      next: function() {
+        return index < length ?
+          { value: collection[index++], done: false } :
+          { done: true };
+      }
+    }
+  };
+
+  has(element) {
+    return this.collection.includes(element);
+  }
+
+  size() {
+    return this.collection.length;
+  }
+
+  clear() {
+    this.collection.length = 0;
+  }
+
+  add(element) {
+    if (!this.has(element)) {
+      this.collection.push(element);
+      return this;
+    }
+    return this;
+  }
 }
